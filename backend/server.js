8d62c67278baab0af0cff9352b9bf2d5f5393eb3
Anurag <21ucc020@lnmiit.ac.in>
2026-03-10 23:42:45 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,8 @@ io.on("connection", (socket) => {
 
 app.set("io", io)
 
-server.listen(5001, () => {
-    console.log("Server running on port 5001")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+ console.log("Server running on", PORT)
 })
